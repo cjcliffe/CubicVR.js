1b945c357316670a210606fdb3781f96683a22c4
Ching Wei Tseng <steven_tseng15@hotmail.com>
2011-09-25 23:36:47 -0400

    Sample Perf test. Capturing FPS is unstable

---
+++ tests/perf/test.js
@@ -0,0 +1,6 @@
+// These are the test files in this dir to be run.
+var tests = [
+  { path: "empty-baseline.pde", tags: ["Perf"] },
+  { path: "background.pde", tags: ["Perf"] },
+  { path: "random-line.pde", tags: ["Perf"] }
+];
