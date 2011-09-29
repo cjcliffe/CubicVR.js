0e40af6574b48ba7a845e3a313bdbb4c248db56c
Steven Tseng <steven_tseng15@hotmail.com>
2011-09-29 13:16:55 -0400

    try to render cube in another js file for 0.2 in branch issue21

---
+++ tests/perf/basic_cube.js
@@ -0,0 +1,19 @@
+// Creates a basic cube in the setup and returns it as a scene object
+// Object will eventually be moved to another file.
+// Create a material for the mesh
+var boxMaterial = new CubicVR.Material({
+    textures: {
+        color: new CubicVR.Texture("../images/6583-diffuse.jpg"),
+        bump: new CubicVR.Texture("../images/6583-bump.jpg")
+    }
+});
+
+// Add a box to mesh, size 1.0, apply material and UV parameters
+boxMesh = CubicVR.primitives.box({
+    size: 1.0,
+    material: boxMaterial,
+    uvmapper: {
+        projectionMode: CubicVR.enums.uv.projection.CUBIC,
+        scale: [1, 1, 1]
+    }
+});
