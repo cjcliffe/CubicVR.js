e5c878d8e0eb477f2ac6bffde2dcd9d8e2923a9e
Steven Tseng <steven_tseng15@hotmail.com>
2011-10-17 16:16:50 -0400

    Formatted the framerate

--- tests/perf/test.js
+++ tests/perf/test.js
@@ -1,6 +1,6 @@
 // These are the test files in this dir to be run.
 var tests = [
   { path: "basic_cube.js", tags: ["Basic"] },
-  { path: "background.pde", tags: ["Perf"] },
+  { path: "sphere_map.js", tags: ["Basic"] },
   { path: "random-line.pde", tags: ["Perf"] }
 ];
