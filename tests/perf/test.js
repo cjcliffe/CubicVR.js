f65e4e4e073539341ead3fc9a0fd925bcc54d144
Steven Tseng <steven_tseng15@hotmail.com>
2011-10-18 13:15:58 -0400

    Added option to choose framerate

--- tests/perf/test.js
+++ tests/perf/test.js
@@ -1,6 +1,5 @@
 // These are the test files in this dir to be run.
 var tests = [
   { path: "basic_cube.js", tags: ["Basic"] },
-  { path: "sphere_map.js", tags: ["Basic"] },
-  { path: "random-line.pde", tags: ["Perf"] }
+  { path: "sphere_map.js", tags: ["Basic"] }
 ];
